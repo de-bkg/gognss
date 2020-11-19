fb9942e3c75b937e145c628d20984839df12cc5e
Erwin Wiesensarter <erwinwiesensarter@web.de>
2020-11-19 11:05:14 +0000

    ADD: build STA-file from sitelogs

---
+++ cmd/sitelogs2sta/main.go
@@ -0,0 +1,65 @@
+// This package reads IGS sitelog files and uses them to generate a Bernese
+// Station Information (STA) file.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/de-bkg/gognss/pkg/site"
+)
+
+const (
+	version = "0.0.1"
+)
+
+func main() {
+	fs := flag.NewFlagSet("sitelog2sta/"+version, flag.ExitOnError)
+	fs.Usage = func() {
+		fmt.Println("sitelog2sta - create a Bernese STA-File based on IGS sitelog formated files")
+		fmt.Println("")
+		fmt.Println("USAGE: sitelog2sta [flags] <sitelogs>")
+		//fmt.Printf("\nFLAGS:\n")
+		//fs.PrintDefaults()
+
+		fmt.Println(`
+EXAMPLES:
+    $ sitelogs2sta ~/sitelogs/*.log >out.sta 2>out.sta.err
+        `)
+
+		fmt.Printf("Version: %s\n", version)
+		fmt.Printf("Source: %s\n", "https://github.com/de-bkg/gognss/tree/master/cmd/sitelog2sta")
+		fmt.Println("BKG Frankfurt 2020, G2")
+	}
+	fs.Parse(os.Args[1:])
+
+	var sites []*site.Site
+	for _, slPath := range fs.Args() {
+		f, err := os.Open(slPath)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		defer f.Close()
+
+		s, err := site.DecodeSitelog(f)
+		if err != nil {
+			log.Printf("decoding sitelog %s: %v", slPath, err)
+			continue
+		}
+
+		err = s.ValidateAndClean(false)
+		if err != nil {
+			log.Printf("validate sitelog %s: %v", slPath, err)
+			continue
+		}
+
+		sites = append(sites, s)
+	}
+
+	err := site.EncodeSTAfile(os.Stdout, sites)
+	if err != nil {
+		log.Fatalf("encode STA-File: %v", err)
+	}
+}
