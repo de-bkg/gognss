ec6053740be43e741a85303974c2e139909a2645
erwiese <erwin.wiesensarter@bkg.bund.de>
2024-07-09 10:47:32 +0000

    ADD: support for version 3.04

--- pkg/rinex/clockdecoder.go
+++ pkg/rinex/clockdecoder.go
@@ -9,18 +9,26 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/de-bkg/gognss/pkg/gnss"
 )
 
 // A ClockHeader stores the RINEX Clock Header information.
 // That header is exposed as the fields of the Decoder and Encoder structs.
 // TODO Parse all fields.
 type ClockHeader struct {
-	RINEXVersion float32 // RINEX Format version
-	RINEXType    string  // RINEX File type
+	RINEXVersion float32     // RINEX Format version
+	RINEXType    string      // RINEX File type
+	SatSystem    gnss.System // Satellite System. System is "Mixed" if more than one.
 
 	Pgm   string    // name of program creating this file
 	RunBy string    // name of agency creating this file
 	Date  time.Time // Date and time of file creation
+
+	TimeSystemID string     // Time system used for time tags, 3 char (GPS, GAL, UTC, TAI,...)
+	AC           string     // Analysis Center as 3-character IGS AC designator
+	NumSolnSats  int        // Number of different satellites in the clock data records.
+	Sats         []gnss.PRN // List of all satellites reported in this file.
 
 	Comments []string // comments
 	Labels   []string // all Header Labels found
@@ -30,7 +38,7 @@ type ClockHeader struct {
 type ClockDecoder struct {
 	// The Header is valid after NewClockDecoder or Reader.Reset. The header must exist,
 	// otherwise ErrNoHeader will be returned.
-	Header  ClockHeader
+	Header  *ClockHeader
 	sc      *bufio.Scanner
 	lineNum int
 	err     error
@@ -46,9 +54,76 @@ func NewClockDecoder(r io.Reader) (*ClockDecoder, error) {
 	return dec, dec.err
 }
 
+// read the RUINEX clock header.
+func (dec *ClockDecoder) readHeader() (*ClockHeader, error) {
+	hdr, err := dec.readHeaderVersion()
+	if err != nil {
+		return nil, err
+	}
+
+	dec.Header = hdr
+
+	if hdr.RINEXVersion >= 3.04 {
+		return dec.readHeader304()
+	}
+	return dec.readHeader300()
+}
+
+// readHeaderVersion reads the first line of the header to get the version, type and satellite system.
+// We need the version for further header parsing.
+func (dec *ClockDecoder) readHeaderVersion() (*ClockHeader, error) {
+	// We need to know the version from the first header line.
+	hdr := &ClockHeader{}
+
+	dec.readLine()
+	line := dec.line()
+	if !strings.Contains(line, "RINEX VERSION") {
+		return nil, fmt.Errorf("parse RINEX VERSION failed: %q", line)
+	}
+
+	// RINEX Version
+	if f64, err := strconv.ParseFloat(strings.TrimSpace(line[:9]), 32); err == nil {
+		hdr.RINEXVersion = float32(f64)
+	} else {
+		return nil, fmt.Errorf("parse RINEX VERSION: %v", err)
+	}
+
+	// RINEX Filetype
+	if hdr.RINEXVersion >= 3.04 {
+		hdr.RINEXType = line[21:22]
+	} else {
+		hdr.RINEXType = line[20:21]
+	}
+
+	if hdr.RINEXType != "C" {
+		return nil, fmt.Errorf("invalid RINEX TYPE: %q", hdr.RINEXType)
+	}
+
+	// Satellite system
+	sys := ""
+	if hdr.RINEXVersion >= 3.04 {
+		sys = strings.TrimSpace(line[42:43])
+	} else {
+		sys = strings.TrimSpace(line[40:41])
+	}
+
+	if sys != "" {
+		if s, ok := sysPerAbbr[sys]; ok {
+			hdr.SatSystem = s
+		} else {
+			return nil, fmt.Errorf("read header: invalid satellite system: %q", sys)
+		}
+	}
+
+	return hdr, nil
+}
+
 // readHeader reads a RINEX clock header. If the Header does not exist,
-// a ErrNoHeader error will be returned.
-func (dec *ClockDecoder) readHeader() (hdr ClockHeader, err error) {
+// an ErrNoHeader will be returned.
+// There was a bigger change introduced in version 3.04.
+func (dec *ClockDecoder) readHeader300() (*ClockHeader, error) {
+	hdr := dec.Header
+
 readln:
 	for dec.readLine() {
 		line := dec.line()
@@ -61,16 +136,6 @@ readln:
 		hdr.Labels = append(hdr.Labels, key)
 
 		switch key {
-		case "RINEX VERSION / TYPE":
-			if f64, err := strconv.ParseFloat(strings.TrimSpace(val[:20]), 32); err == nil {
-				hdr.RINEXVersion = float32(f64)
-			} else {
-				return hdr, fmt.Errorf("parse RINEX VERSION: %v", err)
-			}
-			hdr.RINEXType = val[20:21]
-			if hdr.RINEXType != "C" {
-				return hdr, fmt.Errorf("invalid RINEX TYPE: %q", hdr.RINEXType)
-			}
 		case "PGM / RUN BY / DATE":
 			// Additional lines of this type can appear together after the second line, if needed to preserve the history of previous actions on the file.
 			if hdr.Pgm != "" {
@@ -82,10 +147,24 @@ readln:
 			if date, err := parseHeaderDate(strings.TrimSpace(val[40:])); err == nil {
 				hdr.Date = date
 			} else {
-				log.Printf("parse header date: %q, %v", val[40:], err)
+				log.Printf("parse header date: %v", err)
 			}
+		case "TIME SYSTEM ID":
+			hdr.TimeSystemID = strings.TrimSpace(val[3:6])
 		case "COMMENT":
 			hdr.Comments = append(hdr.Comments, strings.TrimSpace(val))
+		case "ANALYSIS CENTER":
+			hdr.AC = strings.TrimSpace(val[:3])
+		case "# OF SOLN SATS":
+			nSats, err := strconv.Atoi(strings.TrimSpace(val[:6]))
+			if err != nil {
+				return hdr, fmt.Errorf("parse %q: %v", key, err)
+			}
+			hdr.NumSolnSats = nSats
+		case "PRN LIST":
+			if err := dec.parseHeaderPRNList(val); err != nil {
+				return hdr, err
+			}
 		case "END OF HEADER":
 			break readln
 		default:
@@ -97,8 +176,83 @@ readln:
 		return hdr, fmt.Errorf("unknown RINEX Version")
 	}
 
-	err = dec.sc.Err()
+	err := dec.sc.Err()
 	return hdr, err
+}
+
+// readHeader reads a RINEX clock header. If the Header does not exist,
+// a ErrNoHeader error will be returned.
+// There was a bigger change introduced in version 3.04.
+func (dec *ClockDecoder) readHeader304() (*ClockHeader, error) {
+	hdr := dec.Header
+
+readln:
+	for dec.readLine() {
+		line := dec.line()
+		if len(line) < 60 {
+			continue
+		}
+
+		val := line[:65] // RINEX files are ASCII
+		key := strings.TrimSpace(line[65:])
+		hdr.Labels = append(hdr.Labels, key)
+
+		switch key {
+		case "PGM / RUN BY / DATE":
+			// Additional lines of this type can appear together after the second line, if needed to preserve the history of previous actions on the file.
+			if hdr.Pgm != "" {
+				continue
+				// TODO additional lines
+			}
+			hdr.Pgm = strings.TrimSpace(val[:19])
+			hdr.RunBy = strings.TrimSpace(val[21:40])
+			if date, err := parseHeaderDate(strings.TrimSpace(val[42:])); err == nil {
+				hdr.Date = date
+			} else {
+				log.Printf("parse header date: %q, %v", val[42:], err)
+			}
+		case "TIME SYSTEM ID":
+			hdr.TimeSystemID = strings.TrimSpace(val[3:6])
+		case "COMMENT":
+			hdr.Comments = append(hdr.Comments, strings.TrimSpace(val))
+		case "ANALYSIS CENTER":
+			hdr.AC = strings.TrimSpace(val[:3])
+		case "# OF SOLN SATS":
+			nSats, err := strconv.Atoi(strings.TrimSpace(val[:6]))
+			if err != nil {
+				return hdr, fmt.Errorf("parse %q: %v", key, err)
+			}
+			hdr.NumSolnSats = nSats
+		case "PRN LIST":
+			if err := dec.parseHeaderPRNList(val); err != nil {
+				return hdr, err
+			}
+		case "END OF HEADER":
+			break readln
+		default:
+			log.Printf("Header field %q not handled yet", key)
+		}
+	}
+
+	if hdr.RINEXVersion == 0 {
+		return hdr, fmt.Errorf("unknown RINEX Version")
+	}
+
+	err := dec.sc.Err()
+	return hdr, err
+}
+
+// parse header field "PRN LIST".
+func (dec *ClockDecoder) parseHeaderPRNList(s string) error {
+	sats := strings.Fields(s)
+	for _, sat := range sats {
+		prn, err := gnss.NewPRN(sat)
+		if err != nil {
+			return fmt.Errorf("parse %q: %v", "PRN LIST", err)
+		}
+		dec.Header.Sats = append(dec.Header.Sats, prn)
+	}
+	return nil
 }
 
 // Err returns the first non-EOF error that was encountered by the decoder.
@@ -128,3 +282,20 @@ func (dec *ClockDecoder) readLine() bool {
 func (dec *ClockDecoder) line() string {
 	return dec.sc.Text()
 }
+
+// splitAt returns the strings between start/end position pairs in string s, in argument order.
+// Leading and trailing white spaces are trimmed.
+/* func splitAt(s string, pos ...int) []string {
+	fields := []string{}
+
+	for i := 0; i < len(pos); i++ {
+		// Set last position to the end of the string, if out of range.
+		if i == len(pos)-2 && pos[i+1] > len(s) {
+			pos[i+1] = len(s)
+		}
+
+		fields = append(fields, strings.TrimSpace(s[pos[i]:pos[i+1]]))
+		i++
+	}
+	return fields
+} */
--- pkg/rinex/clockdecoder_test.go
+++ pkg/rinex/clockdecoder_test.go
@@ -3,7 +3,9 @@ package rinex
 import (
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/de-bkg/gognss/pkg/gnss"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -45,6 +47,94 @@ AR AIRA 2023 10 24 00 00  0.000000  2   -3.167986726646e-08  3.643193552360e-12
 	assert.Equal("C", hdr.RINEXType)
 	assert.Equal("CCLOCK", hdr.Pgm)
 	assert.Equal("IGSACC @ GA MIT", hdr.RunBy)
-	//assert.Equal(time.Date(2019, 11, 3, 0, 7, 0, 0, time.UTC), hdr.Date)
+	assert.Equal("IGS", hdr.AC)
+}
+
+func TestClockDecoder_readHeader304(t *testing.T) {
+	const header = `3.04                 C                    M                      RINEX VERSION / TYPE
+CCRNXC V5.3          AIUB                 21-AUG-20 05:54        PGM / RUN BY / DATE
+Center for Orbit Determination in Europe (CODE)                  COMMENT
+Final GNSS clock information for year-day 2020-229               COMMENT
+Clock information consistent with phase and C1W/C2W code data    COMMENT
+Satellite/receiver clock values at intervals of 5/300 sec        COMMENT
+High-rate (5 sec) clock interpolation based on phase data        COMMENT
+Product reference: DOI 10.7892/boris.75876.4                     COMMENT
+   GPS                                                           TIME SYSTEM ID
+    18                                                           LEAP SECONDS GNSS
+G  CLKEST V5.3        IGS14                                      SYS / PCVS APPLIED
+R  CLKEST V5.3        IGS14                                      SYS / PCVS APPLIED
+G  CLKEST V5.3        CODE.BIA @ ftp.aiub.unibe.ch/CODE/         SYS / DCBS APPLIED
+R  CLKEST V5.3        CODE.BIA @ ftp.aiub.unibe.ch/CODE/         SYS / DCBS APPLIED
+     2    AR    AS                                               # / TYPES OF DATA
+COD  Center for Orbit Determination in Europe                    ANALYSIS CENTER
+     1                                                           # OF CLK REF
+TIDB00AUS 50103M108                           0.000000000000E+00 ANALYSIS CLK REF
+   306    IGb14                                                  # OF SOLN STA / TRF
+TIDB00AUS 50103M108           -4460996987  2682557086 -3674442611SOLN STA NAME / NUM
+ZIMM00CHE 14001M004            4331296853   567556159  4633134121SOLN STA NAME / NUM
+    53                                                           # OF SOLN SATS
+G01 G02 G03 G04 G05 G06 G07 G08 G09 G10 G11 G12 G13 G15 G16 G17  PRN LIST
+G18 G19 G20 G21 G22 G24 G25 G26 G27 G28 G29 G30 G31 G32 R01 R02  PRN LIST
+R03 R04 R05 R07 R08 R09 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20  PRN LIST
+R21 R22 R23 R24 R26                                              PRN LIST
+                                                                 END OF HEADER
+	`
+
+	prnListWanted := []gnss.PRN{{Sys: gnss.SysGPS, Num: 1}, {Sys: gnss.SysGPS, Num: 2}, {Sys: gnss.SysGPS, Num: 3},
+		{Sys: gnss.SysGPS, Num: 4}, {Sys: gnss.SysGPS, Num: 5}, {Sys: gnss.SysGPS, Num: 6}, {Sys: gnss.SysGPS, Num: 7},
+		{Sys: gnss.SysGPS, Num: 8}, {Sys: gnss.SysGPS, Num: 9}, {Sys: gnss.SysGPS, Num: 10}, {Sys: gnss.SysGPS, Num: 11},
+		{Sys: gnss.SysGPS, Num: 12}, {Sys: gnss.SysGPS, Num: 13}, {Sys: gnss.SysGPS, Num: 15}, {Sys: gnss.SysGPS, Num: 16},
+		{Sys: gnss.SysGPS, Num: 17}, {Sys: gnss.SysGPS, Num: 18}, {Sys: gnss.SysGPS, Num: 19}, {Sys: gnss.SysGPS, Num: 20},
+		{Sys: gnss.SysGPS, Num: 21}, {Sys: gnss.SysGPS, Num: 22}, {Sys: gnss.SysGPS, Num: 24}, {Sys: gnss.SysGPS, Num: 25},
+		{Sys: gnss.SysGPS, Num: 26}, {Sys: gnss.SysGPS, Num: 27}, {Sys: gnss.SysGPS, Num: 28}, {Sys: gnss.SysGPS, Num: 29},
+		{Sys: gnss.SysGPS, Num: 30}, {Sys: gnss.SysGPS, Num: 31}, {Sys: gnss.SysGPS, Num: 32}, {Sys: gnss.SysGLO, Num: 1},
+		{Sys: gnss.SysGLO, Num: 2}, {Sys: gnss.SysGLO, Num: 3}, {Sys: gnss.SysGLO, Num: 4}, {Sys: gnss.SysGLO, Num: 5},
+		{Sys: gnss.SysGLO, Num: 7}, {Sys: gnss.SysGLO, Num: 8}, {Sys: gnss.SysGLO, Num: 9}, {Sys: gnss.SysGLO, Num: 11},
+		{Sys: gnss.SysGLO, Num: 12}, {Sys: gnss.SysGLO, Num: 13}, {Sys: gnss.SysGLO, Num: 14}, {Sys: gnss.SysGLO, Num: 15},
+		{Sys: gnss.SysGLO, Num: 16}, {Sys: gnss.SysGLO, Num: 17}, {Sys: gnss.SysGLO, Num: 18}, {Sys: gnss.SysGLO, Num: 19},
+		{Sys: gnss.SysGLO, Num: 20}, {Sys: gnss.SysGLO, Num: 21}, {Sys: gnss.SysGLO, Num: 22}, {Sys: gnss.SysGLO, Num: 23},
+		{Sys: gnss.SysGLO, Num: 24}, {Sys: gnss.SysGLO, Num: 26}}
+
+	assert := assert.New(t)
+	dec, err := NewClockDecoder(strings.NewReader(header))
+	assert.NoError(err)
+	assert.NotNil(dec)
+
+	hdr := dec.Header
+	assert.Equal(float32(3.04), hdr.RINEXVersion)
+	assert.Equal("C", hdr.RINEXType)
+	assert.Equal(hdr.SatSystem, gnss.SysMIXED)
+	assert.Equal("CCRNXC V5.3", hdr.Pgm)
+	assert.Equal("AIUB", hdr.RunBy)
+	assert.Equal("COD", hdr.AC)
+	assert.Equal("GPS", hdr.TimeSystemID)
+	assert.Equal(53, hdr.NumSolnSats)
+	assert.Equal(time.Date(2020, 8, 21, 5, 54, 0, 0, time.UTC), hdr.Date)
+	assert.Equal(prnListWanted, hdr.Sats, "PRN List")
+
 	//t.Logf("RINEX Header: %+v\n", hdr)
 }
+
+/* func Test_splitAt(t *testing.T) {
+	type args struct {
+		s   string
+		pos []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "t1-304", args: args{s: "3.04                 C                    M                      ", pos: []int{0, 4, 21, 22, 42, 43}},
+			want: []string{"3.04", "C", "M"}},
+		{name: "t1-304-2", args: args{s: "3.04                 C                    M                      ", pos: []int{0, 4, 21, 22, 42, 99}},
+			want: []string{"3.04", "C", "M"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitAt(tt.args.s, tt.args.pos...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitAt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+} */
